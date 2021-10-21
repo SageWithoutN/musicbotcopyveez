9ccf52a5bc3df24fdb635aded043b951cbb9be97
SageWithoutN <92230729+SageWithoutN@users.noreply.github.com>
2021-10-21 17:07:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -13,11 +13,11 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Veez Music")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/20147c4f049e2c1f2f248.png")
-THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/4c39fbb88932761913fff.png")
-AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/73e10ed6e2bd32b478de6.png")
-QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/b95c13eef1ebd14dbb458.png")
-CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c83b000f004f01897fe18.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://telegra.ph/file/c432fff9dc76d21c47d30.jpg")
+AUD_IMG = getenv("AUD_IMG", "https://telegra.ph/file/c432fff9dc76d21c47d30.jpg")
+QUE_IMG = getenv("QUE_IMG", "https://telegra.ph/file/c432fff9dc76d21c47d30.jpg")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/c432fff9dc76d21c47d30.jpg")
+ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/c432fff9dc76d21c47d30.jpg")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_USERNAME = getenv("BOT_USERNAME", "veezmusicbot")
@@ -34,7 +34,7 @@ DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
 # make a private channel and get the channel id
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 # just fill with True or False (optional)
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "True"))
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
@@ -43,6 +43,6 @@ U_BRANCH = "main"
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_REPO = os.environ.get(
-    "UPSTREAM_REPO", "https://github.com/levina-lab/VeezMusic"
+    "UPSTREAM_REPO", "https://github.com/SageWithoutN/musicbotcopyveez"
 )
 HEROKU_URL = fetch_heroku_git_url(HEROKU_API_KEY, HEROKU_APP_NAME)
